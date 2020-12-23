fbc63f358e61bbb561d7b6448094858ce8ac9491
Your Name <you@example.com>
2020-12-24 00:21:52 +0300

    commit2

--- index.php
+++ index.php
@@ -3,14 +3,22 @@
     <title>PHP</title>
 </head>
 <body>
-    <?php
-
-        echo "Hello World!<br />";
-        echo "Hello World!";
-
-
-
-
-    ?>
+    <form metod="GET" action="index.php">
+        <p>
+            Name
+        </p>
+        <input type="text" name="name">
+        <p>
+            Password
+        </p>
+        <input type="text" name="password">
+        <br />
+        <input type="submit">
+    </form>
 </body>
 </html>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+echo '<h1>Привет, <b>' . $_GET['name'] . $_GET['password'] . '</b></h1>!';
+}
+?>
